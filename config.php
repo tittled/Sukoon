ae475e3928cfb7d4c668f933ae6694dcb7662976
tittled <titled.official@proton.me>
2025-06-24 14:37:33 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// --- IMPORTANT SECURITY NOTE ---
+// This file contains your secret API key.
+// Do NOT commit this file to a public repository (like GitHub).
+// If you use Git, you should add "config.php" to your .gitignore file.
+
+// Place your actual YouTube Data API v3 key here.
+define('YOUTUBE_API_KEY', 'YOUR_API_KEY');
+
+?>
